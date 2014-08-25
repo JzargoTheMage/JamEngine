e30fef59edf65aab2183dd4ce51f7cbe50e7ee88
JzargoTheMage <JzargoTheMage@gmail.com>
2014-08-25 14:26:16 -0500

    Got bored, fixed a possible problem.

--- boxes.py
+++ boxes.py
@@ -2,9 +2,9 @@
 box = 0
 playAgain = 1
 print "Boxes!"
-box = int(raw_input("Open one? Type a number."))
 while playAgain == 1:
+    box = int(raw_input("Open one? Type a number."))
     while box != 0:
-        print "You acutally opened one? Wow, you deserve a medal!"
+        print "You acutally opened one? You have no life."
         playAgain = int(raw_input("Are you done yet? Type 1 for no and any other number for yes."))
         box = 0
