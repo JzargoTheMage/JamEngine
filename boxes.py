fe326c4ddaff2d40bca78ea92c83f29940c8c798
JzargoTheMage <JzargoTheMage@gmail.com>
2014-09-05 14:01:22 -0500

    Update boxes.py
    Replace some 'while' with 'if'

--- boxes.py
+++ boxes.py
@@ -4,7 +4,7 @@ playAgain = 1
 print "Boxes!"
 while playAgain == 1:
     box = int(raw_input("Open one? Type a number."))
-    while box != 0:
+    if box != 0:
         print "You acutally opened one? You have no life."
         playAgain = int(raw_input("Are you done yet? Type 1 for no and any other number for yes."))
         box = 0
