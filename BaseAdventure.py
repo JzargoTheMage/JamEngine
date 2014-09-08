7d973ebcc38401eddd0b928c2960daa4ba570b84
James <JzargoTheMage@gmail.com>
2014-09-08 14:25:40 -0500

    Update BaseAdventure.py
    Brought in shops

--- BaseAdventure.py
+++ BaseAdventure.py
@@ -1,2 +1,34 @@
 #!/usr/bin/env python
-#Merge the other programs into one file here.
+print "Welcome!"
+Filler = 0
+Area = 0
+Health = 10
+Attack = 3
+Defense = 5
+Coins = 15
+ShopMenu = 0
+playAgain = 1
+while playAgain == 1:
+    while Area == 0:
+        print "What would you like to do?"
+        Area = int(raw_input("Type 1 to go shopping, or 2 to proceed on your journey.  ")
+    while Area == 1:
+        if ShopMenu == 0:
+            print "Welcome to the shop! What would you like? Upgrades are 5 coins."
+            ShopMenu = int(raw_input("1 is Attack, 2 is Defense, 3 is Health. Type 4 to return to town center.  ")
+        if ShopMenu == 1:
+            Attack = Attack + 2
+            ShopMenu = 0
+        if ShopMenu == 2:
+            Defense = Defense +2
+            ShopMenu = 0
+        if ShopMenu == 3:
+            Health = Health +5
+            ShopMenu = 0
+        if ShopMenu == 4:
+            Area = 0
+            ShopMenu = 0
+    while Area == 2:
+        print "Go away."
+        Area = 0
+#Need to add the battle and treasure options into this
